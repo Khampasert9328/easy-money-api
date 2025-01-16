efaeee952e9c579a05c354e8309eb7ff65405b86
Xaiy_IT <109733313+Khampasert9328@users.noreply.github.com>
2025-01-16 10:58:16 +0700

    commit
    update

---
+++ src/config/index.js
@@ -0,0 +1,5 @@
+require('dotenv').config().parsed
+
+module.exports ={
+    JWT_SECRET: process.env.JWT_TOKEN,
+}
--- src/routes/payments/payments_router.js
+++ src/routes/payments/payments_router.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
+
+//passportJWT
+const isAuth = require('../../middleware/passportJWt')
 const payments  = require('../../controllers/payment/payments_controllers');
 
 //route
-router.post('/createPayments', payments.createPayment);
-router.get('/getPayments', payments.getPayment);
-router.get('/payments/:paymentsid', payments.getPaymentById);
-router.put('/payments/:paymentsid', payments.updatePayment);
-router.delete('/payments/:paymentsid', payments.removePayment);
+router.post('/createPayments',[isAuth.isLogin], payments.createPayment);
+router.get('/getPayments',[isAuth.isLogin], payments.getPayment);
+router.get('/payments/:paymentsid',[isAuth.isLogin], payments.getPaymentById);
+router.put('/payments/:paymentsid',[isAuth.isLogin], payments.updatePayment);
+router.delete('/payments/:paymentsid',[isAuth.isLogin], payments.removePayment);
 module.exports = router;
